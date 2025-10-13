b2a8e57a55d4d17666463aad19b9d597e9194e45
Mario Hewardt <marioh@microsoft.com>
2025-10-13 09:32:35 -0700

    Update jcd to version 1.0.1 with new URL and SHA256 (#8)

--- Formula/jcd.rb
+++ Formula/jcd.rb
@@ -2,9 +2,9 @@ class Jcd < Formula
     desc "jcd (jump change directory) is a command-line tool that provides enhanced directory navigation with substring matching and smart selection. It's like the cd command, but with superpowers! jcd is part of the Sysinternals tool suite."
     homepage "https://learn.microsoft.com/en-us/sysinternals/downloads/jcd"
   
-    url "https://github.com/microsoft/jcd/releases/download/1.0.0/jcd-mac-1.0.0.zip"
-    sha256 "b6b970461d2e70d9e962e778767f186d000f5c2c88ad94757717715ff757d6b1"  
-    version "1.0.0"
+    url "https://github.com/microsoft/jcd/releases/download/1.0.1/jcd-mac-1.0.1.zip"
+    sha256 "9bbfc30d2c65f053545a3c272dd2421c2213bfa6aac02f0cc44328f216a8ba5c"  
+    version "1.0.1"
     license "MIT"
 
     def install
