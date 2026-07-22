0b822d147bfa137b283bdd617e2c7764c5b4e51c
Mario Hewardt <marioh@microsoft.com>
2026-07-22 09:28:14 -0700

    Create ZoomIt cask for installation (#14)
    Add ZoomIt cask for macOS with version 12.2.0.

---
+++ Casks/zoomit.rb
@@ -0,0 +1,17 @@
+cask "zoomit" do
+  version "12.2.0"
+  sha256 "b629ebc9f4e7531acb7545c4c38b9843ecdea57771909a81e88806e23020e5ae"
+
+  url "https://github.com/microsoft/ZoomitForMac/releases/download/#{version}/ZoomIt-12.2.0.dmg"
+  name "ZoomIt"
+  desc "Screen zoom, annotation, capture, and recording utility"
+  homepage "https://github.com/microsoft/ZoomitForMac"
+
+  depends_on macos: ">= :sonoma"
+
+  app "ZoomIt.app"
+
+  uninstall quit: "com.sysinternals.zoomitmac"
+
+  zap trash: "~/Library/Preferences/com.sysinternals.zoomitmac.plist"
+end
